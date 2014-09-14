149bce9f9ce7824596aa236548eb4d5a73c08d45
Darren O'Connor <mellow.drifter@gmail.com>
2014-09-14 12:55:13 +0100

    definiation start

--- ic.py
+++ ic.py
@@ -21,4 +21,31 @@
 7 : lowerLayerDown ===
 '''
 
-import pysnmp
+from pysnmp.entity.rfc3413.oneliner import cmdgen
+
+
+interface = []
+def interface_list(x):
+    cmdGen = cmdgen.CommandGenerator()
+    errorIndication, errorStatus, errorIndex, varBindTable = cmdGen.nextCmd(
+            cmdgen.CommunityData('public'),
+            cmdgen.UdpTransportTarget(('172.16.64.50', 161)),
+            '1.3.6.1.2.1.2.2.1.2',
+    )
+
+    if errorIndication:
+        print(errorIndication)
+    else:
+        if errorStatus:
+            print('%s at %s' % (
+                errorStatus.prettyPrint(),
+                errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'
+                )
+            )
+        else:
+            for varBindTableRow in varBindTable:
+                for name, val in varBindTableRow:
+                    print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
+                    interface.append[val.prettyPrint()]
+    return interface
+
